8a8b946ff427476ecb6a4ed52be8d681f795b1cd
Ihimbru-k <kanihims25@gmail.com>
2023-07-15 12:21:46 +0100

    blink led with LDR

---
+++ blink_led_with_LDR/blink_led_with_LDR.ino
@@ -0,0 +1,20 @@
+const int ledPin = 13;
+const int sensorPin = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int rate = AnalogRead(sensorPin);
+  Serial.println(rate);
+  rate = map(rate,200,800,minDuration,maxDuration);
+  digitalWrite(ledPin, HIGH);
+  delay(rate);
+  digitalWrite(ledPin, LOW);
+  delay(rate);
+
+}
